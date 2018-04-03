1b9d66ad3f3108546b3d762f6c7200f96bdcf3ba
kaizengjun <kaizengjun@163.com>
2018-04-04 07:22:49 +0800

    Add watcher.js and entrance file

--- src/compile.js
+++ src/compile.js
@@ -1,5 +1,7 @@
-function Compile(el) {
+function Compile(el, vm) {
+    this.$vm = vm;
     this.$el = this.isElementNode(el) ? el : document.querySelector(el);
+
     if (this.$el) {
         this.$fragment = this.node2Fragment(this.$el);
         this.init();
@@ -10,17 +12,23 @@ Compile.prototype = {
     init: function() {
         this.compileElement(this.$fragment);
     },
+
     node2Fragment: function(el) {
         var child;      
         var fragment = document.createDocumentFragment();
+
+        // 将原生节点拷贝到fragment
         while (child = el.firstChild) {
             fragment.appendChild(child);
         }
+
         return fragment;
     },
+
     compileElement: function(el) {
-        var childNode = el.childNodes;
+        var childNodes = el.childNodes;
         var me = this;
+
         [].slice.call(childNodes).forEach(function(node) {
             var text = node.textContent;
             var reg = /\{\{(.*)\}\}/;   // 表达式文本
@@ -36,9 +44,11 @@ Compile.prototype = {
             }
         })
     },
+
     compile: function(node) {
         var nodeAttrs = node.attributes;
         var me = this;
+
         [].slice.call(nodeAttrs).forEach(function(attr) {
             // 规定： 指令以 v-xxx 命名
             // 如 <span v-text="content"></span> 中指令为 v-text
@@ -53,24 +63,103 @@ Compile.prototype = {
                     // 普通指令
                     compileUtil[dir] && compileUtil[dir](node, me.$vm, exp);
                 }
+
+                node.removeAttribute(attrName);
             }
         });
+    },
+
+    compileText: function(node, exp) {
+        compileUtil.text(node, this.$vm, exp);
+    },
+
+    isDirective: function(attr) {
+        return attr.indexOf('v-') === 0;
+    },
+
+    isEventDirective: function(dir) {
+        return dir.indexOf('on') === 0;
+    },
+
+    isElementNode: function(node) {
+        return node.nodeType === 1;
+    },
+
+    isTextNode: function(node) {
+        return node.nodeType === 3;
     }
 };
 
 // 指令处理集合
 var compileUtil = {
     text: function(node, vm, exp) {
-        this.bind(node, vm, exp, 'text')
+        this.bind(node, vm, exp, 'text');
     },
+
+    html: function(node, vm, exp) {
+        this.bind(node, vm, exp, 'html');
+    },
+
+    model: function(node, vm, exp) {
+        this.bind(node, vm, exp, 'model');
+
+        var me = this;
+        var val = this._getVMVal(vm, exp);
+
+        node.addEventListener('input', function(e) {
+            var newValue = e.target.value;
+            if (val === newValue) {
+                return;
+            }
+            me._setVMVal(vm, exp, newValue);
+            val = newValue;
+        });
+    },
+
+    class: function(node, vm, exp) {
+        this.bind(node, vm, exp, 'class');
+    },
+
     bind: function(node, vm, exp, dir) {
         var updaterFn = updater[dir + 'Updater'];
         // 第一次初始化试图
-        updaterFn && updaterFn(node, vm[exp]);
+        updaterFn && updaterFn(node, this._getVMVal(vm, exp));
         // 实例化订阅者，此操作会在对应的属性消息订阅器中添加了该订阅者watcher
         new Watcher(vm, exp, function(value, oldValue) {
-          // 一旦属性值有变化， 会收到通知执行此更新函数，更新视图
-          updateFn && updateFn(node, value, oldValue);
+            // 一旦属性值有变化， 会收到通知执行此更新函数，更新视图
+            updaterFn && updaterFn(node, value, oldValue);
+        });
+    },
+
+    // 事件处理
+    eventHandler: function(node, vm, exp, dir) {
+        var eventType = dir.split(':')[1];
+        var fn = vm.$options.methods && vm.$options.methods[exp];
+
+        if (eventType && fn) {
+            node.addEventListener(eventType, fn.bind(vm), false);
+        }
+    },
+
+    _getVMVal: function(vm, exp) {
+        var val = vm;
+        exp = exp.split('.');
+        exp.forEach(function(k) {
+            val = val[k];
+        });
+        return val;
+    },
+
+    _setVMVal: function(vm, exp, value) {
+        var val = vm;
+        exp = exp.split('.');
+        exp.forEach(function(k, i) {
+            // 非最后一个key, 更新val的值
+            if (i < exp.length - 1) {
+                val = val[k];
+            } else {
+                val[k] = value;
+            }
         })
     }
 };
@@ -78,6 +167,23 @@ var compileUtil = {
 // 更新函数
 var updater = {
     textUpdater: function(node, value) {
-        node.textContent = typeof value === 'undefined' ? '' : value;
+        node.textContent = typeof value == 'undefined' ? '' : value;
+    },
+
+    htmlUpdater: function(node, value) {
+        node.innerHTML = typeof value == 'undefined' ? '' : value;
+    },
+
+    classUpdater: function(node, value, oldValue) {
+        var className = node.className;
+        className = className.replace(oldValue, '').replace(/\s$/, '');
+
+        var space = className && String(value) ? ' ' : '';
+        
+        node.className = className + space + value;
+    },
+
+    modelUpdater: function(node, value, oldValue) {
+        node.value = typeof value == 'undefined' ? '' : value;
     }
 }
